585f08cdb413ba1cf7cfb08c6993ee6dd01e685e
Nastya Reznyakova <reznyakovanastya@gmail.com>
2026-04-02 16:46:33 -0400

    Add Day 18 practice: regular expressions and pattern matching

--- day_18_regular_expressions.py
+++ day_18_regular_expressions.py
@@ -18,9 +18,42 @@ def ex1():
     
     result.sort(reverse=True)
     return result
-    
-print("EXERSICE 1: ")
+
+
+print("EXERSICE 1:")
 for i in ex1():
     print(i)
-    
-                    
+
+#ex2
+def ex2():
+    points = ['-12', '-4', '-3', '-1', '0', '4', '8']
+    regex_pattern = r'-?\d+'
+    found_points = [int(num) for num in points]
+
+    sorted_points = sorted(found_points)
+    distance = max(sorted_points) - min(sorted_points)
+    print(sorted_points)
+    return distance
+print("EXERSICE 2:", ex2())
+
+#ex3 
+def ex3(text):
+    pattern = r'^[A-Za-z_][A-Za-z0-9_]*$'
+    return bool(re.match(pattern,text))
+
+print("EXERSICE 3:", ex3('firstname'))
+
+#ex4
+def ex4():
+    sentence = '''%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?'''
+    found_list = re.findall(r'[^a-zA-Z\s]',sentence)
+    result = []
+    for i in found_list:
+        if i in result:
+            result[0] += 1
+        else: 
+            result[0] = 1
+            result[1] = i
+    result.sort(reverse=True)
+    return result
+print("EXERSICE 4:", ex4())
